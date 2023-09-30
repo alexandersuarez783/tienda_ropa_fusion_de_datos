53dd47291df1c5a4a11986518a47dc4760f2f9f3
neondreams <neondreams7734@gmail.com>
2023-09-29 21:40:08 -0500

    added two more regression models

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from matplotlib import pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn import linear_model
+
 
 # generate data if it does not exist
 data_generator = DatasetGenerator()
@@ -31,9 +33,24 @@ X_train, X_test, y_train, y_test = get_train_and_test_data()
 reg = LinearRegression()
 reg.fit(X_train, y_train)
 
-prediction_difference = (reg.predict(X_test) - y_test)
-mean_squared_error = sum(prediction_difference ** 2) / len(prediction_difference)
-print(f'Mean squared error: {mean_squared_error:.2f}')
+prediction_difference_linear = (reg.predict(X_test) - y_test)
+mean_squared_error_linear = sum(prediction_difference_linear ** 2) / len(prediction_difference_linear)
+print(f'Mean squared error: {mean_squared_error_linear:.2f}')
+
+# ridge
+ridge = linear_model.Ridge(alpha=.5)
+ridge.fit(X_train, y_train)
+prediction_difference_ridge = (ridge.predict(X_test) - y_test)
+mean_squared_error_ridge = sum(prediction_difference_ridge ** 2) / len(prediction_difference_ridge)
+print(f'Mean squared error ridge: {mean_squared_error_ridge:.2f}')
+
+# bayesian ridge
+bayesian = linear_model.BayesianRidge()
+bayesian.fit(X_train, y_train)
+prediction_difference_bayesian = (bayesian.predict(X_test) - y_test)
+mean_squared_error_ridge = sum(prediction_difference_bayesian ** 2) / len(prediction_difference_bayesian)
+print(f'Mean squared error bayesian: {mean_squared_error_ridge:.2f}')
+
 
 # pca
 reg_pca = LinearRegression()
